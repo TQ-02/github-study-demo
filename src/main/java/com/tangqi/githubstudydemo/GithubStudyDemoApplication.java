bd7bcf5abf9b411b13ae0be51bf061f4dd5727c5
TQ <2538657321@qq.com>
2025-04-18 14:10:36 +0800

    Initial commit

---
+++ src/main/java/com/tangqi/githubstudydemo/GithubStudyDemoApplication.java
@@ -0,0 +1,16 @@
+package com.tangqi.githubstudydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tangqi
+ */
+@SpringBootApplication
+public class GithubStudyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubStudyDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tangqi/githubstudydemo/GithubStudyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangqi.githubstudydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubStudyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
