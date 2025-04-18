8df83380db25df4b4d0f32e0ebeb33adf8b43c51
TQ <2538657321@qq.com>
2025-04-18 17:45:53 +0800

    feat(utils): 新增了一个加法方法

---
+++ src/main/java/com/tangqi/githubstudydemo/utils/CalculationUtil.java
@@ -0,0 +1,13 @@
+package com.tangqi.githubstudydemo.utils;
+
+/**
+ * @author: tangqi
+ * @CreateTime: 2025-04-18  17:42
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class CalculationUtil {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
