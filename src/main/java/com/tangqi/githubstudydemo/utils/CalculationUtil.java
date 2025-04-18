247e34a58263f9ffeefb6b548260f401e838daf9
TQ <2538657321@qq.com>
2025-04-18 17:51:41 +0800

    feat(utils): 新增了一个乘法方法

--- src/main/java/com/tangqi/githubstudydemo/utils/CalculationUtil.java
+++ src/main/java/com/tangqi/githubstudydemo/utils/CalculationUtil.java
@@ -10,4 +10,8 @@ public class CalculationUtil {
     public static int add(int a, int b) {
         return a + b;
     }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
 }
